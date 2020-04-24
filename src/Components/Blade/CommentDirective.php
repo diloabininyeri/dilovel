9a5d3ca09d9eb5602712dba5174f125936a53b97
dılo sürücü <berxudar@gmail.com>
2020-04-24 18:59:41 +0300

    singleton trait created for  singleton

---
+++ src/Components/Blade/CommentDirective.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Components\Blade;
+
+
+use Blade\BladeDirectiveInterface;
+
+class CommentDirective implements BladeDirectiveInterface
+{
+
+    public function getDirectiveRegexPattern()
+    {
+
+    }
+
+    public function replaceTemplate(string $template)
+    {
+        // TODO: Implement replaceTemplate() method.
+    }
+}
