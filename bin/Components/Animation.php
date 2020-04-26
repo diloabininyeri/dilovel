c7e65cca6134cee5f90abf1a7341263aeb0b7f77
dılo sürücü <berxudar@gmail.com>
2020-04-26 18:52:20 +0300

    command stub created

--- bin/Components/Animation.php
+++ bin/Components/Animation.php
@@ -4,8 +4,15 @@
 namespace Bin\Components;
 
 
+/**
+ * Class Animation
+ * @package Bin\Components
+ */
 class Animation
 {
+    /**
+     *
+     */
     public static function show(): void
     {
         foreach (range(1, 180) as $i) {
