37e341d9fcbdbbcbbe38edc3c1cc67b676cef37a
dılo sürücü <berxudar@gmail.com>
2020-04-26 18:05:41 +0300

    recomended command made php console make:xxx

--- bin/Components/CustomCommandCall.php
+++ bin/Components/CustomCommandCall.php
@@ -56,8 +56,39 @@ abstract class  CustomCommandCall
             (new $commandClass())->handle(array_slice($this->signals, 1));
 
         } else {
-            echo 'Command Not found';
+            echo $this->createNotFoundMessage($this->findSimilarCommand($this->signals[0]));
         }
+
+    }
+
+
+    /**
+     * @param $command
+     * @return mixed
+     */
+    private function findSimilarCommand($command)
+    {
+        $similarCommands = $this->sortSimilarity($command);
+        return $similarCommands[0]['command'];
+
+    }
+
+    /**
+     * @param $command
+     * @return array[]
+     */
+    private function sortSimilarity($command): array
+    {
+        $similarRate = array_map(static function ($cmd) use ($command) {
+            return
+                [
+                    'similarity' => similar_text($cmd, $command),
+                    'command' => $cmd
+                ];
+        }, array_keys($this->getCommands()));
+
+        uasort($similarRate, fn($cmd, $cmd1) => -($cmd['similarity'] <=> $cmd1['similarity']));
+        return array_values($similarRate);
 
     }
 
@@ -71,6 +102,12 @@ abstract class  CustomCommandCall
             return $this->commands;
         }
         return $this->commands[$key] ?? null;
+    }
+
+
+    private function createNotFoundMessage($command): string
+    {
+        return "command not found but you can use $command";
     }
 
 
