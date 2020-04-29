654240edbb5e561303783046fd4305d49e28036a
dılo sürücü <berxudar@gmail.com>
2020-04-29 20:49:54 +0300

    router global helper tested for generator url

--- src/Routers/web.php
+++ src/Routers/web.php
@@ -1,11 +1,12 @@
 <?php
 
-Router::get('/home/ff/id',fn()=>router('deneme',['id'=>14]))->name('deneme')->middleware('mid','mid2');
+Router::get('/home/ff/:id',fn()=>router('deneme',['id'=>14]))->name('deneme')->middleware('mid','mid2');
 
 
 Router::get('deneme/:id',static function (){
 
-    return request()->get('id');
+    $id= request()->get('id');
+    return router('deneme',['id'=>$id]);
 
 });
 Router::get('/fefe','controller@dsffssffs')->name('deneme1')->middleware('mid','mid2');
