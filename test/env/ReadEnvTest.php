79b8cc2b780e1ab1d7a8c83d9a258546a7f244d1
dılo sürücü <berxudar@gmail.com>
2020-04-29 20:32:58 +0300

    wrote toarray to json for env file read

--- test/env/ReadEnvTest.php
+++ test/env/ReadEnvTest.php
@@ -4,15 +4,50 @@
 use App\Components\Env\EnvFile;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class ReadEnvTest
+ */
 class ReadEnvTest extends TestCase
 {
 
+    /**
+     * @var EnvFile
+     */
+    private $envFile;
+
+    /**
+     *
+     */
+    public function setUp(): void
+    {
+        $this->envFile = new EnvFile('.env');
+        parent::setUp();
+    }
+
+    /**
+     *
+     */
     public function testGetValue()
     {
-        $file = new EnvFile('.env');
-        $value = $file->getValue('TEST_EXAMPLE');
+        $value = $this->envFile->getValue('TEST_EXAMPLE');
 
         $this->assertEquals(123456, $value);
     }
 
+    /**
+     *
+     */
+    public function testToArray()
+    {
+        $this->assertIsArray($this->envFile->toArray());
+    }
+
+
+    /**
+     * @throws JsonException
+     */
+    public function testToJson()
+    {
+        $this->assertJson($this->envFile->toJson());
+    }
 }
