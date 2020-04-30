25cee3a1e1301c5dc0187ccb353ae7df64e46ccf
dılo sürücü <berxudar@gmail.com>
2020-05-01 00:53:23 +0300

    comment added to error active function

--- index.php
+++ index.php
@@ -5,8 +5,14 @@ use App\Bootstrap\App;
 
 require_once 'vendor/autoload.php';
 
+/**
+ * activates errors to see errors in the app
+ */
 activate_errors();
 
+/**
+ *
+ */
 echo (new App())
     ->run()
     ->call404IfNotFound();
