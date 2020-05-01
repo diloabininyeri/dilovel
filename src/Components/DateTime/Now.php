bab9c828f709ad5cbe1585b40dddcafbc5aa5734
dılo sürücü <berxudar@gmail.com>
2020-05-01 21:35:36 +0300

    created Now class

---
+++ src/Components/DateTime/Now.php
@@ -0,0 +1,131 @@
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+
+namespace App\Components\DateTime;
+
+use DateInterval;
+use DateTime;
+use Exception;
+
+/**
+ * Class Now
+ * @author  dılo sürücü <berxudar@gmail.com>
+ */
+class Now
+{
+
+    /**
+     * @var DateTime
+     */
+    private $dateTime;
+
+    /**
+     * Now constructor.
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $this->dateTime = new DateTime('now');
+    }
+
+
+    /**
+     * @param int $year
+     * @return Now
+     * @throws Exception
+     * @noinspection PhpUnused
+     */
+    public function addYear(int $year): self
+    {
+        $this->dateTime->add(new DateInterval('P' . $year . 'Y'));
+
+        return $this;
+    }
+
+    /**
+     * @noinspection PhpUnused
+     * @param int $month
+     * @return Now
+     * @throws Exception
+     * @noinspection PhpUnused
+     */
+    public function addMonth(int $month):self
+    {
+        $this->dateTime->add(new DateInterval('P' . $month . 'M'));
+
+        return $this;
+    }
+
+    /**
+     * @param int $day
+     * @return $this
+     * @throws Exception
+     */
+    public function addDay(int $day): self
+    {
+        $this->dateTime->add(new DateInterval('P' . $day . 'D'));
+        return $this;
+    }
+
+    /**
+     * @noinspection PhpUnused
+     * @param int $week
+     * @return $this
+     * @throws Exception
+     */
+    public function addWeek(int $week): self
+    {
+        return $this->addDay($week * 7);
+    }
+
+    /**
+     * @noinspection PhpUnused
+     * @param int $second
+     * @return $this
+     * @throws Exception
+     */
+    public function addSecond(int $second): self
+    {
+        $this->dateTime->add(new DateInterval('PT' . $second . 'S'));
+        return $this;
+    }
+
+    /**
+     * @param int $minute
+     * @return $this
+     * @throws Exception
+     */
+    public function addMinute(int $minute): self
+    {
+        $this->dateTime->add(new DateInterval('PT' . $minute . 'M'));
+        return $this;
+    }
+
+    /**
+     * @param int $hour
+     * @return $this
+     * @throws Exception
+     */
+    public function addHour(int $hour): self
+    {
+        $this->dateTime->add(new DateInterval('PT' . $hour . 'H'));
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function get(): string
+    {
+        return $this->dateTime->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->get();
+    }
+
+}
