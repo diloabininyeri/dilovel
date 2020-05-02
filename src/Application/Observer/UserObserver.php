8440d91df4255b4a1cb231f7868c460be7ec653d
dılo sürücü <berxudar@gmail.com>
2020-05-02 21:53:30 +0300

    userobserver class implemented interface him

--- src/Application/Observer/UserObserver.php
+++ src/Application/Observer/UserObserver.php
@@ -4,7 +4,29 @@
 namespace App\Application\Observer;
 
 
-class UserObserver
+use App\Components\Database\Model;
+use App\Interfaces\ModelObserverInterface;
+
+class UserObserver implements ModelObserverInterface
 {
 
+    public function updated(Model $model)
+    {
+        // TODO: Implement updated() method.
+    }
+
+    public function deleted(Model $model)
+    {
+        // TODO: Implement deleted() method.
+    }
+
+    public function inserted(Model $model)
+    {
+        // TODO: Implement inserted() method.
+    }
+
+    public function saved(Model $model)
+    {
+        // TODO: Implement saved() method.
+    }
 }
