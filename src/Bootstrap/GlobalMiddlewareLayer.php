11e9a73a349446e322c8e1d48913648f565497eb
dılo sürücü <berxudar@gmail.com>
2020-05-02 23:16:57 +0300

    phpdoc added some method and variable for example @see for goto defined

--- src/Bootstrap/GlobalMiddlewareLayer.php
+++ src/Bootstrap/GlobalMiddlewareLayer.php
@@ -11,6 +11,7 @@ use Closure;
  * Class GlobalMiddlewareLayer
  * @package App\Bootstrap
  * @property-read $global
+ * @see Middleware::$global get $this->global variable from Middleware class with  call closure event protected variable
  */
 class GlobalMiddlewareLayer
 {
@@ -30,7 +31,7 @@ class GlobalMiddlewareLayer
 
                 if ($request instanceof Request) {
                     $request = (new $middleware)->handle(fn($response) => $response, $request);
-                } else {break; }
+                } else {break;}
             }
 
             return $request;
