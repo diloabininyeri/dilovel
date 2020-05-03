77d02479b46e655251182a4039a8eceaf1b2b823
dılo sürücü <berxudar@gmail.com>
2020-05-03 22:33:12 +0300

    watcher.sh was not working solved wather.sh

--- watcher.sh
+++ watcher.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 sudo apt install inotify-tools
 while inotifywait -e modify src/logs/error.log; do
-  if tail -n1 src/logs/error.log | grep apache; then
     php console error:detected
     kdialog --msgbox "error log file changed"
-  fi
 done
