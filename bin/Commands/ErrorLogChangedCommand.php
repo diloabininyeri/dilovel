61f01ed81b11d2d70dc3f5765bb64e1fd597419e
dılo sürücü <berxudar@gmail.com>
2020-05-03 21:38:12 +0300

    ErrorLogChangedCommand::description explained

--- bin/Commands/ErrorLogChangedCommand.php
+++ bin/Commands/ErrorLogChangedCommand.php
@@ -6,12 +6,25 @@ namespace Bin\Commands;
 
 use Bin\Components\CommandInterface;
 
+/**
+ * Class ErrorLogChangedCommand
+ * @package Bin\Commands
+ */
 class ErrorLogChangedCommand implements CommandInterface
 {
 
+    /**
+     * @var string
+     */
+    protected string  $description='auto-trigger when an error occurs, no need to call manually';
+
+    /**
+     * @param array|null $parameters
+     */
     public function handle(?array $parameters): void
     {
 
+        //send mail to admin an error occur
 
     }
 }
