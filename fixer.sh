2f9ce28631d26214b5b3987c3e44316d7861fbea
dılo sürücü <berxudar@gmail.com>
2020-05-03 23:06:41 +0300

    @@PHP71Migration and 73 added to fixer.sh

--- fixer.sh
+++ fixer.sh
@@ -1,2 +1,4 @@
- php-cs-fixer fix . --rules=@PSR1
- php-cs-fixer fix . --rules=@PSR2
+php-cs-fixer fix . --rules=@PSR1
+php-cs-fixer fix . --rules=@PSR2
+php-cs-fixer fix . --rules=@PHP71Migration
+php-cs-fixer fix . --rules=@PHP73Migration
