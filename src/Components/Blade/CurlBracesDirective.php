c69c7413cfe12f17263534a8118e72d9420861a7
dılo sürücü <berxudar@gmail.com>
2020-05-07 13:51:28 +0300

    {{}} directive in template engine can avoid html avoid js escape and can inject variable maybe ı thinking make filter

--- src/Components/Blade/CurlBracesDirective.php
+++ src/Components/Blade/CurlBracesDirective.php
@@ -10,7 +10,7 @@ class CurlBracesDirective implements BladeDirectiveInterface
      */
     public function getDirectiveRegexPattern()
     {
-        return '/[^@]{{(.*)}}/sU';
+        return '/.?{{(.*)}}/sU';
     }
 
     /**
@@ -18,8 +18,27 @@ class CurlBracesDirective implements BladeDirectiveInterface
      */
     public function replaceTemplate(string $template)
     {
-        return preg_replace_callback($this->getDirectiveRegexPattern(), static function ($find) {
-            return '<?php echo htmlspecialchars(' . $find[1] .');?>';
+        return preg_replace_callback($this->getDirectiveRegexPattern(), function ($find) {
+            $firstLetter=$this->getFirstLetter($find);
+
+            if ($firstLetter === '@') {
+                return $find[0];
+            }
+
+            if ($firstLetter !== ' ') {
+                return $firstLetter . '<?php echo htmlspecialchars(' . $find[1] . ');?>';
+            }
+
+            return '<?php echo htmlspecialchars(' . $find[1] . ');?>';
         }, $template);
+    }
+
+    /**
+     * @param $find
+     * @return mixed
+     */
+    private function getFirstLetter($find)
+    {
+        return $find[0][0];
     }
 }
