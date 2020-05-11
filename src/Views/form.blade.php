cd4c54dd0fbf4c7f532066fda056a85bea547055
dılo sürücü <berxudar@gmail.com>
2020-05-11 04:24:26 +0300

    form.blade tested with bootstrap for withError method

--- src/Views/form.blade.php
+++ src/Views/form.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -8,12 +10,31 @@
     <title>Document</title>
 </head>
 <body>
-@foreach ($errors as $error)
-    <h2>{{$error}}</h2>
-@endforeach
-<form action="{{router('form.post')}}" enctype="multipart/form-data" method="post">
-    <input type="file" name="file">
-    <input type="submit" value="gonder">
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <img class="img-thumbnail" src="{{assets('images/2020_05_11_file5eb8a286a5e531.84547494.png')}}" alt="">
+            @foreach ($errors as $error)
+                <div class="alert alert-danger">{{$error}}</div>
+            @endforeach
+            <form action="{{router('form.post')}}" enctype="multipart/form-data" method="post">
+                <div class="form-group">
+
+                    <label for="exampleFormControlFile1">Example file input</label>
+                    <input type="file" class="form-control-file" name="file">
+                </div>
+                <div class="form-group">
+                    <input class="form-control btn btn-info" type="submit" value="gonder">
+                </div>
+
+
+            </form>
+        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+    </div>
+</div>
+
 </body>
 </html>
