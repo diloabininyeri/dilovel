d12398ab397ba90523b9c155ea76c7a146fdcbe0
dılo sürücü <berxudar@gmail.com>
2020-06-08 04:30:38 +0300

    migrations:publish optimized

--- bin/Commands/PublishMigrationToDbCommand.php
+++ bin/Commands/PublishMigrationToDbCommand.php
@@ -6,11 +6,13 @@ namespace Bin\Commands;
 use App\Components\Database\Migration\CallMigrationObjects;
 use App\Components\Database\Migration\MigrationStorage;
 use App\Components\Database\PDOAdaptor;
+use Bin\Components\ColorConsole;
 use Bin\Components\CommandInterface;
 
 class PublishMigrationToDbCommand implements CommandInterface
 {
     protected string $description = 'create all tables from migrations';
+
 
     public function handle(?array $parameters): void
     {
@@ -18,30 +20,31 @@ class PublishMigrationToDbCommand implements CommandInterface
         $migrations = MigrationStorage::all();
         $tables = array_keys($migrations);
 
+        $this->createTable($tables, $migrations);
+
+    }
+
+    private function createTable(array $tables, array $migrations): void
+    {
+        $withoutLengths = ['TEXT', 'LONGTEXT'];
+
         foreach ($tables as $table) {
-            $sql = "CREATE TABLE $table(";
+
+            $sql = "CREATE TABLE IF NOT EXISTS $table (";
             foreach ($migrations[$table] as $key => $value) {
-                foreach ($value as $itemKey => $itemValue) {
-                    if ($itemKey === 'column_name') {
-                        $columnName = $itemValue;
-                    }
-                    if ($itemKey === 'length') {
-                        $length = $itemValue;
-                    }
-                    if ($itemKey === 'type') {
-                        $type = $itemValue;
-                    }
-                }
-                if (!in_array($type, ['TEXT', 'LONGTEXT'])) {
-                    $sql .= "$columnName $type($length),";
+                if (in_array($value['type'], $withoutLengths, true)) {
+                    $sql .= sprintf('%s %s,', $value['column_name'], $value['type']);
                 } else {
-                    $sql .= "$columnName $type,";
+                    $sql .= sprintf('%s %s(%d),', $value['column_name'], $value['type'], $value['length']);
                 }
             }
             $sql = rtrim($sql, ',');
-            $sql .= ")";
+            $sql .= ')';
 
-            echo PDOAdaptor::connection($migrations[$table][0]['connection_name'])->exec($sql) ?: "$table created" . PHP_EOL;
+
+            echo PDOAdaptor::connection($migrations[$table][0]['connection_name'])->exec($sql) ?: ColorConsole::getInstance()->getColoredString("$table migrated\n", 'green');
+
+
         }
     }
 }
