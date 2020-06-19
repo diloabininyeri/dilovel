e066318fde86c3f55ee021d6c9316c2e35c886c5
dılo sürücü <berxudar@gmail.com>
2020-06-19 21:55:21 +0300

    maincache class created

--- src/Components/Cache.php
+++ src/Components/Cache.php
@@ -3,6 +3,30 @@
 
 namespace App\Components;
 
+use App\Components\Cache\MainCache;
+use App\Components\Cache\Redis\Redis;
+use Closure;
+
+/**
+ * Class Cache
+ * @package App\Components
+ * @method static remember($key, Closure $closure)
+ * @method static get(string $key, $default = null)
+ * @method static set(string $key, $value)
+ * @method static setex(string $key, $value, $time = 120)
+ * @method static bool exists(string $key)
+ *
+ */
 class Cache
 {
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        return (new MainCache())->$name(...$arguments);
+    }
 }
---
+++ src/Components/Cache/MainCache.php
@@ -0,0 +1,85 @@
+<?php
+
+
+namespace App\Components\Cache;
+
+use App\Components\Cache\Redis\Redis;
+use Closure;
+
+/**
+ * Class MainCache
+ * @package App\Components\Cache
+ */
+class MainCache
+{
+    /**
+     * @var Redis
+     */
+    private Redis $redis;
+
+    /**
+     * MainCache constructor.
+     */
+    public function __construct()
+    {
+        $this->redis = Redis::connection();
+    }
+
+
+    /**
+     * @param string $name
+     * @param Closure $closure
+     * @param int $second
+     * @return mixed
+     */
+    public function remember(string $name, Closure $closure, int $second = 120)
+    {
+        if ($this->redis->exists($name)) {
+            return unserialize($this->redis->get($name), ['allowed_classes' => true]);
+        }
+        $data = $closure();
+        $this->redis->setex($name, $second, serialize($data));
+        return $data;
+    }
+
+    /**
+     * @param string $key
+     * @param null $default
+     * @return string|null
+     */
+    public function get(string $key, $default=null): ?string
+    {
+        return $this->redis->get($key) ?? $default;
+    }
+
+    /**
+     * @param string $key
+     * @param $value
+     * @return mixed
+     */
+    public function set(string $key, $value)
+    {
+        return $this->redis->set($key, $value);
+    }
+
+
+    /**
+     * @param string $key
+     * @param $value
+     * @param int $time
+     * @return int
+     */
+    public function setex(string $key, $value, $time=120): int
+    {
+        return $this->redis->setex($key, $time, $value);
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function exists(string $key):bool
+    {
+        return $this->redis->exists($key);
+    }
+}
