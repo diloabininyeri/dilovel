1d476cb1319f6e8ebeaf33e16bd5d48a1c6df9ea
dılo sürücü <berxudar@gmail.com>
2020-06-21 01:59:31 +0300

    array_push_between created for adds inserted in bloog etc

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -55,6 +55,16 @@ function view($file, $params = [])
     return (new View($file, $params))->compile();
 }
 
+/**
+ * @param array $array
+ * @param $item
+ * @param int $index
+ * @return array
+ */
+function array_push_between(array $array,$item,int $index)
+{
+    return array_merge(array_slice($array,0,$index),[$item],array_slice($array,$index));
+}
 
 /**
  * @param string $connectionName
