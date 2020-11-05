906e726209c91aad5c6eedb8159cbe89cd3f3863
dılo sürücü <berxudar@gmail.com>
2020-11-05 12:37:28 +0300

    throttle middleware created for 60 seconds 60 attempt

---
+++ src/Application/Middleware/ThrottleMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Application\Middleware;
+
+use App\Components\Http\Request;
+use App\Interfaces\MiddlewareInterface;
+use Closure;
+use Throttle\Throttle;
+use Throttle\Time;
+
+/**
+ * Class ThrottleMiddleware
+ * @package App\Application\Middleware
+ */
+class ThrottleMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * @param Closure $next
+     * @param Request $request
+     * @return mixed|void
+     */
+    public function handle(Closure $next, Request $request)
+    {
+        $throttle = new Throttle(new Time(60, 60));
+        $throttle->commit();
+        if ($throttle->isHasAccessLimit()) {
+            return $next($request);
+        }
+        return abort(404);
+    }
+}
