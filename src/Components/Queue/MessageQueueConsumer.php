fb92e324ee49fc1e8c606227e85ef1df0c54a602
dılo sürücü <berxudar@gmail.com>
2020-11-15 00:06:54 +0300

    queue global helper binded to MessageQueue component

--- src/Components/Queue/MessageQueueConsumer.php
+++ src/Components/Queue/MessageQueueConsumer.php
@@ -21,4 +21,25 @@ class MessageQueueConsumer
     {
         return new MessageQueueConsumerFactory($queueName, $microSleep);
     }
+
+    /**
+     * @param string $queueName
+     * @param int|null $sleepTime
+     * @throws ErrorException
+     */
+    public static function listen(string $queueName, ?int $sleepTime=null): void
+    {
+        (new MessageQueueConsumerFactory($queueName, $sleepTime))->listen();
+    }
+
+    /**
+     * @param string $name
+     * @param Closure $closure
+     * @param $microSleep
+     * @throws ErrorException
+     */
+    public static function listenWithCallback(string $name, Closure $closure, $microSleep): void
+    {
+        (new MessageQueueConsumerFactory($name, $microSleep))->with($closure);
+    }
 }
