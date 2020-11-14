f261b698fa87d1b609c1b51f55e46e59c2b2529d
dılo sürücü <berxudar@gmail.com>
2020-11-15 00:45:01 +0300

    queue system tested and changed somewhere

--- bin/Commands/QueueListenCommand.php
+++ bin/Commands/QueueListenCommand.php
@@ -6,23 +6,34 @@ namespace Bin\Commands;
 use App\Components\Queue\MessageQueueConsumer;
 use App\Interfaces\QueueInterface;
 use Bin\Components\CommandInterface;
+use ErrorException;
 use Exception;
 
+/**
+ * Class QueueListenCommand
+ * @package Bin\Commands
+ */
 class QueueListenCommand implements CommandInterface
 {
+    /**
+     * @var string
+     */
     protected string $description = 'listen the queue default queue name is default';
 
-    /** @noinspection UnserializeExploitsInspection */
+    /**
+     * @param array|null $parameters
+     * @throws ErrorException
+     */
     public function handle(?array $parameters): void
     {
-        $queue=$parameters[0] ?? 'default';
+        $queue = $parameters[0] ?? 'default';
         MessageQueueConsumer::run($queue)->with(static function ($a) {
             /**
              * @var QueueInterface $obj
              */
-            $obj=unserialize($a);
+            $obj = unserialize($a, ['allowed_class'=>true]);
             try {
-                echo $obj->handle().PHP_EOL;
+                echo $obj->handle() . PHP_EOL;
             } catch (Exception $exception) {
                 $obj->failed($exception);
             }
